fb8ba53f3737863855f8fb10fe2cd22a0b8772c4
chaenni <chaenni@hsr.ch>
2017-12-04 11:55:21 +0100

    für de remulus zum bastle

--- tic-tac-toe-bot.hs
+++ tic-tac-toe-bot.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 data Marker = X | O | Empty
 
 instance Eq Marker where
@@ -9,9 +11,6 @@ type Row = (Marker, Marker, Marker)
 type Field = (Row, Row, Row)
 
 data Winner = None | Won Marker
-
-instance Show (Won) where
-  show a = a
 
 allEq a b c = a == b && b == c
 
@@ -28,3 +27,33 @@ won ((a1, a2, a3),
                  | allEq a1 b2 c3 = Won a1
                  | allEq a3 b2 c1 = Won a3
                  | otherwise = None
+
+type Position = (Int, Int)
+
+charToIntX :: Char -> Maybe Int
+charToIntX a 
+           | a == 'a' = Just 1
+           | a == 'b' = Just 2
+           | a == 'c' = Just 3
+           | otherwise = Nothing
+
+charToIntY :: Char -> Maybe Int
+charToIntY a 
+           | a == '1' = Just 1
+           | a == '2' = Just 2
+           | a == '3' = Just 3
+           | otherwise = Nothing
+
+{-
+getPosition = do 
+  x <- getChar
+  y <- getChar
+  xi <- charToIntX x
+  yi <- charToIntY y
+  if xi \= Nothing && yi \= Nothing then
+    return (fromJust xi, fromJust yi)
+  else
+    do
+      putStrLn "Invalid entry, try again:"
+      getPosition
+      -}
